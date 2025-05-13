e865c76c4a5b4111da1b98129942d47c69061d08
O`brian <obrian@sooho.io>
2025-05-13 16:30:25 +0900

    Update dom.ts

--- src/dom.ts
+++ src/dom.ts
@@ -204,7 +204,7 @@ export class QRCanvas {
   }
 }
 
-class QRCamera {
+export class QRCamera {
   private stream: MediaStream;
   private player: HTMLVideoElement;
   constructor(stream: MediaStream, player: HTMLVideoElement) {
